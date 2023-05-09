184f5924717cbdb70c03d08b714738b374ce4cfd
Teja <103510023+NaveentejaB@users.noreply.github.com>
2023-05-10 01:05:55 +0530

    first commit

--- mod1.mjs
+++
@@ -1,5 +0,0 @@
-import dz , {d,a,b}  from './mod2.mjs'
-console.log(dz);
-console.log(a);
-console.log(b);
-console.log(d);
--- mod2.mjs
+++
@@ -1,9 +0,0 @@
-const a ="aa";
-const b ="bb";
-const c = "cc";
-const d ="dd";
-
-export default c;
-export {a};
-export {b};
-export {d};
